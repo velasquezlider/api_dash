1c446bf4a6db295e491a0b1e859801226b685aa1
Vismark1234 <129228402+Vismark1234@users.noreply.github.com>
2026-02-27 10:21:02 -0400

    Actualización de consultas SQL

--- sql.sql
+++ sql.sql
@@ -1,4 +1,15 @@
 --1 
+/*
+
+KPI-E1: TASA DE ENTREGA EXITOSA (TEE)
+  Fórmula    : (Paquetes entregados / Total paquetes admitidos) × 100
+  Fuente     : packages.ESTADO = 'ENTREGADO' / COUNT(packages)
+  Meta       : ≥ 92%
+  Frecuencia : Mensual / Trimestral
+  Alerta     : < 85% = Nivel Crítico
+  Descripción: Mide la eficacia global del sistema postal. Es el KPI más
+               representativo de la calidad del servicio.
+*/
 
 SELECT 
     COUNT(*) AS total_paquetes,
@@ -23,4 +34,22 @@ KPI-E2: TASA DE REZAGO (TR)
   Descripción: Mide el porcentaje de paquetes que superan el plazo máximo
                permitido sin ser entregados o devueltos.
 */
+
+SELECT 
+    COUNT(*) AS total_paquetes,
+    SUM(CASE 
+            WHEN ESTADO = 'REZAGO' 
+            THEN 1 ELSE 0 
+        END) AS paquetes_rezago,
+    ROUND(
+        (SUM(CASE 
+                WHEN ESTADO = 'REZAGO'  
+                THEN 1 ELSE 0 
+            END) / COUNT(*)) * 100
+    ,2) AS TR_porcentaje
+FROM packages
+WHERE YEAR(created_at) = 2024
+  AND MONTH(created_at) = 5;
+
+
 
