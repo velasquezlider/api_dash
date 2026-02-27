28dc414247f580237839fafd51cf4b22ce693d79
Vismark1234 <129228402+Vismark1234@users.noreply.github.com>
2026-02-27 10:06:29 -0400

    Actualización

--- sql.sql
+++ sql.sql
@@ -1,1 +1,10 @@
 --1 
+SELECT 
+    COUNT(*) AS total_paquetes,
+    SUM(CASE WHEN ESTADO = 'ENTREGADO' THEN 1 ELSE 0 END) AS entregados,
+    ROUND(
+        (SUM(CASE WHEN ESTADO = 'ENTREGADO' THEN 1 ELSE 0 END) / COUNT(*)) * 100,
+        2
+    ) AS TEE_porcentaje
+FROM packages
+WHERE YEAR(created_at) = 2024;
